d6a2d3289c95b06ab2432f6f47095cb6946e3ef7
Thomaz Botelho <thomazrb@gmail.com>
2023-08-26 15:48:25 -0300

    Exemplos de dados.

---
+++ dados.py
@@ -0,0 +1,12 @@
+def modificar_valor(lista):
+    lista.append(4)
+
+minha_lista = [1, 2, 3]
+print(type(minha_lista))
+print(minha_lista)
+modificar_valor(minha_lista)
+print(minha_lista)
+
+x = 5
+x = x + 1
+print(x)
