46622874ce3df84d1c2ea07774ad7b344d4daa53
Thomaz Botelho <thomazrb@gmail.com>
2023-08-26 15:39:54 -0300

    Exemplos de entrada de dados

---
+++ entrada.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome:")
+print('Olá', nome)
+
+idade_str = input("Digite sua idade: ")
+print(type(idade_str))
+idade = int(idade_str)
+print(type(idade))
+print("Idade:", idade)
