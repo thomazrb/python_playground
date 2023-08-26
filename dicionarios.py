4cb820ce71b307616e4fcfad34f6258d231253d2
Thomaz Botelho <thomazrb@gmail.com>
2023-08-26 16:28:57 -0300

    Exemplo de dicionário.

---
+++ dicionarios.py
@@ -0,0 +1,15 @@
+# lista = []
+# tuplas = ()
+# dicionários = {} conjunto chave-valor
+
+meu_dicionario = {
+    "nome": "Zezinho",
+    "idade": 15,
+    "Cidade": "Vitória"
+}
+
+print(meu_dicionario["idade"])
+meu_dicionario["Altura"] = 1.62
+print(meu_dicionario)
+
+
