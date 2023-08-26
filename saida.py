d8e000ec83209a8e756fffbde74fa511b6dec2f5
Thomaz Botelho <thomazrb@gmail.com>
2023-08-26 15:27:51 -0300

    exemplos de saída com Python

--- saida.py
+++ saida.py
@@ -1,1 +1,30 @@
 print("Hello World!")
+
+nome = "Zezinho"
+idade = 10
+print("Nome:", nome, "Idade:", idade)
+x = "Nome:", nome, "Idade:", idade
+print(type(x))
+
+print('Nome: {0} Idade: {1}'.format(nome, idade))
+y = 'Nome: {0} Idade: {1}'.format(nome, idade)
+print(type(y))
+
+print(f'Nome: {nome} Idade: {idade}')
+
+
+print('Ola', end=' ')
+print('Mundo')
+
+valor = 123.234215
+print('valor = {0:.2f}'.format(valor))
+
+
+print(f'\033[92m Oi\033[0m')
+
+x = """Oi
+
+Teste
+!"""
+
+print(x)
